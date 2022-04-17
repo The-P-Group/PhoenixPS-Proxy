1a2b5422852b5d2157fe050c431bf8a4ff53e6a7
phuchptty <13364457+phuchptty@users.noreply.github.com>
2022-04-17 23:45:01 +0700

    feat: using uriBuilder

--- PPS Proxy/Program.cs
+++ PPS Proxy/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System.Net;
-using System.Security.Cryptography.X509Certificates;
 using Titanium.Web.Proxy;
 using Titanium.Web.Proxy.EventArguments;
 using Titanium.Web.Proxy.Models;
@@ -7,51 +6,45 @@ using Titanium.Web.Proxy.Models;
 var proxyServer = new ProxyServer();
 
 // locally trust root certificate used by this proxy 
-//proxyServer.CertificateManager.TrustRootCertificate(true);
-
-// optionally set the Certificate Engine
-// Under Mono only BouncyCastle will be supported
-//proxyServer.CertificateManager.CertificateEngine = Network.CertificateEngine.BouncyCastle;
+proxyServer.CertificateManager.CertificateEngine = Titanium.Web.Proxy.Network.CertificateEngine.DefaultWindows;
+proxyServer.CertificateManager.EnsureRootCertificate();
+proxyServer.CertificateManager.TrustRootCertificate(true);
 
 Task OnRequest(object sender, SessionEventArgs e)
 {
-    //Console.WriteLine(e.HttpClient.Request.Url);
-
     string[] listDomain = { "mihoyo", "hoyoverse", "yuanshen", "18.222.82.186:22401" };
 
-    // To cancel a request with a custom HTML content
-    // Filter URL
-
+    // TODO: Improve this fukking logic
     for (int i = 0; i < listDomain.Length; i++)
     {
         if (e.HttpClient.Request.RequestUri.AbsoluteUri.Contains(listDomain[i]))
         {
-            var https = e.HttpClient.Request.IsHttps;
-
-            var hostUrl = e.HttpClient.Request.Host;
-            var fullUrl = e.HttpClient.Request.Url.ToString();
-
-            var newUrl = fullUrl.Replace(hostUrl, "localhost");
-
-            if (https)
-            {
-                newUrl = newUrl.Replace("https", "http");
-            }
 
 #if DEBUG
             var method = e.HttpClient.Request.Method.ToUpper();
             if (method == "POST")
             {
-
                 Console.WriteLine(e.GetRequestBodyAsString().Result);
             }
 #endif
 
+            UriBuilder uriBuilder = new UriBuilder();
+
+            uriBuilder.Scheme = "http";
+
+            uriBuilder.Host = "localhost";
+
+            uriBuilder.Path = e.HttpClient.Request.RequestUriString;
+
+            Uri uri = uriBuilder.Uri;
 
             //Console.WriteLine(newUrl);
 
-            Console.WriteLine("Redirect from " + hostUrl + " to localhost");
-            e.Redirect(newUrl);
+            var hostUrl = e.HttpClient.Request.Host;
+            Console.WriteLine("Redirect " + hostUrl + " to localhost");
+
+            e.Redirect(uri.ToString());
+
         }
     }
 
@@ -67,11 +60,7 @@ var explicitEndPoint = new ExplicitProxyEndPoint(IPAddress.Any, 8080, true)
     // Optimizes performance by not creating a certificate for each https-enabled domain
     // Useful when certificate trust is not required by proxy clients
     //GenericCertificate = new X509Certificate2(Path.Combine(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), "genericcert.pfx"), "password")
-    //GenericCertificate = new X509Certificate2("E:/PROJECT/GENSHIN_STUFF/pps/data/cert/PPS_Cert.crt")
 };
-
-// Fired when a CONNECT request is received
-//explicitEndPoint.BeforeTunnelConnectRequest += OnBeforeTunnelConnectRequest;
 
 proxyServer.AddEndPoint(explicitEndPoint);
 proxyServer.Start();
@@ -84,13 +73,13 @@ foreach (var endPoint in proxyServer.ProxyEndPoints)
 proxyServer.SetAsSystemHttpProxy(explicitEndPoint);
 proxyServer.SetAsSystemHttpsProxy(explicitEndPoint);
 
-// wait here (You can use something else as a wait function, I am using this as a demo)
+// wait here (You can use something else as a wait function)
 Console.Read();
-
-// Unsubscribe & Quit
 
 proxyServer.BeforeRequest -= OnRequest;
 
 
 proxyServer.Stop();
+
+// Restore original proxy setting
 proxyServer.RestoreOriginalProxySettings();
