9b4ba12b3d10de1b81540b7b19d27d38d363b35c
phuchptty <13364457+phuchptty@users.noreply.github.com>
2022-04-25 23:03:35 +0700

    feat: create app config & remove fukup logic

--- PPS Proxy/Program.cs
+++ PPS Proxy/Program.cs
@@ -1,86 +1,74 @@
 ﻿using System.Net;
+using System.Configuration;
+using System.Linq;
 using Titanium.Web.Proxy;
 using Titanium.Web.Proxy.EventArguments;
 using Titanium.Web.Proxy.Models;
 
-var proxyServer = new ProxyServer();
-
-// locally trust root certificate used by this proxy 
-proxyServer.CertificateManager.CertificateEngine = Titanium.Web.Proxy.Network.CertificateEngine.DefaultWindows;
-proxyServer.CertificateManager.EnsureRootCertificate();
-proxyServer.CertificateManager.TrustRootCertificate(true);
+var forwardUrl = ConfigurationManager.AppSettings["FORWARD_URL"] ?? "http://localhost";
+var listenPort = Int32.Parse(ConfigurationManager.AppSettings["PROXY_LISTEN_PORT"] ?? "8080");
+var parseBody = bool.Parse(ConfigurationManager.AppSettings["PARSE_BODY"] ?? "false");
 
 Task OnRequest(object sender, SessionEventArgs e)
 {
     string[] listDomain = { "mihoyo", "hoyoverse", "yuanshen", "18.222.82.186:22401" };
 
-    // TODO: Improve this fukking logic
-    for (int i = 0; i < listDomain.Length; i++)
+    if (listDomain.Any(e.HttpClient.Request.RequestUri.AbsoluteUri.Contains))
     {
-        if (e.HttpClient.Request.RequestUri.AbsoluteUri.Contains(listDomain[i]))
+
+        if (parseBody && e.HttpClient.Request.Method.ToUpper() == "POST")
         {
-
-#if DEBUG
-            var method = e.HttpClient.Request.Method.ToUpper();
-            if (method == "POST")
-            {
-                Console.WriteLine(e.GetRequestBodyAsString().Result);
-            }
-#endif
-
-            var https = e.HttpClient.Request.IsHttps;
-
-            var hostUrl = e.HttpClient.Request.Host;
-            var fullUrl = e.HttpClient.Request.Url.ToString();
-
-            var newUrl = fullUrl.Replace(hostUrl, "localhost");
-
-            if (https)
-            {
-                newUrl = newUrl.Replace("https", "http");
-            }
-
-            //Console.WriteLine(newUrl);
-
-            Console.WriteLine("Redirect " + hostUrl + " to localhost");
-
-            e.Redirect(newUrl);
-
+            Console.WriteLine(e.GetRequestBodyAsString().Result);
         }
+
+        var hostUrl = e.HttpClient.Request.Host is not null ? e.HttpClient.Request.Host : e.HttpClient.Request.RequestUri.Host;
+
+        var abPath = e.HttpClient.Request.RequestUri.AbsolutePath;
+
+        e.HttpClient.Request.RequestUri = new Uri(forwardUrl + abPath);
+
+        Console.WriteLine("Redirected {0} to {1}", hostUrl, forwardUrl.Replace("http://", ""));
     }
 
     return Task.CompletedTask;
 }
 
-proxyServer.BeforeRequest += OnRequest;
-
-
-var explicitEndPoint = new ExplicitProxyEndPoint(IPAddress.Any, 8080, true)
+void Main()
 {
-    // Use self-issued generic certificate on all https requests
-    // Optimizes performance by not creating a certificate for each https-enabled domain
-    // Useful when certificate trust is not required by proxy clients
-    //GenericCertificate = new X509Certificate2(Path.Combine(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), "genericcert.pfx"), "password")
-};
+    var proxyServer = new ProxyServer();
 
-proxyServer.AddEndPoint(explicitEndPoint);
-proxyServer.Start();
+    // locally trust root certificate used by this proxy 
+    proxyServer.CertificateManager.CertificateEngine = Titanium.Web.Proxy.Network.CertificateEngine.DefaultWindows;
+    proxyServer.CertificateManager.EnsureRootCertificate();
+    proxyServer.CertificateManager.TrustRootCertificate(true);
 
-foreach (var endPoint in proxyServer.ProxyEndPoints)
-    Console.WriteLine("Listening on '{0}' endpoint at Ip {1} and port: {2} ",
-        endPoint.GetType().Name, endPoint.IpAddress, endPoint.Port);
+    proxyServer.BeforeRequest += OnRequest;
 
-// Only explicit proxies can be set as system proxy!
-proxyServer.SetAsSystemHttpProxy(explicitEndPoint);
-proxyServer.SetAsSystemHttpsProxy(explicitEndPoint);
+    var explicitEndPoint = new ExplicitProxyEndPoint(IPAddress.Any, listenPort, true) { };
 
-// wait here (You can use something else as a wait function)
-Console.Read();
+    proxyServer.AddEndPoint(explicitEndPoint);
+    proxyServer.Start();
 
-proxyServer.BeforeRequest -= OnRequest;
+    foreach (var endPoint in proxyServer.ProxyEndPoints)
+    {
+        Console.WriteLine("Listening on '{0}' endpoint at Ip {1} and port: {2} ",
+            endPoint.GetType().Name, endPoint.IpAddress, endPoint.Port);
+    }
 
+    // Only explicit proxies can be set as system proxy!
+    proxyServer.SetAsSystemHttpProxy(explicitEndPoint);
+    proxyServer.SetAsSystemHttpsProxy(explicitEndPoint);
 
-proxyServer.Stop();
+    Console.WriteLine("Press any to stop program !");
+    Console.Read();
 
-// Restore original proxy setting
-proxyServer.RestoreOriginalProxySettings();
+    proxyServer.BeforeRequest -= OnRequest;
+
+    proxyServer.Stop();
+
+    // Restore original proxy setting
+    proxyServer.RestoreOriginalProxySettings();
+
+}
+
+Main();
